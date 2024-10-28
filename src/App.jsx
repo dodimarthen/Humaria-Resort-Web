856961a6cf42692f394f3705a2469c960f2ac485
dodimarthen <dodimartin.sit@gmail.com>
2024-10-28 09:32:28 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+        <div>hello jings</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
