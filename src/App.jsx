c35b7d95a42afc1e6425852df6b82625f4d786c4
dodimarthen <dodimartin.sit@gmail.com>
2024-10-28 10:11:02 +0700

    create background

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 function App() {
   return (
     <>
-      <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-        <div>hello jings</div>
-      </div>
+      <div></div>
     </>
   );
 }
