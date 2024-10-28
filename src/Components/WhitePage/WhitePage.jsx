6cc4bc06b260590f52eb708c522418249dfdb128
dodimarthen <dodimartin.sit@gmail.com>
2024-10-28 14:42:39 +0700

    create whitepage

---
+++ src/Components/WhitePage/WhitePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WhitePage = () => {
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <p className="text-gray-800 text-2xl">This is a blank white page</p>
+    </div>
+  );
+};
+
+export default WhitePage;
