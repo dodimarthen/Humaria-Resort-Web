389e2b2bc09547500c40c00a6f1d484ccb417f83
dodimarthen <dodimartin.sit@gmail.com>
2024-10-28 14:43:11 +0700

    generate layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import React from "react";
+import Header from "./Components/Header/Header";
+import WhitePage from "./Components/WhitePage/WhitePage";
+import Navbar from "./Components/Navbar/navbar";
+
 function App() {
   return (
     <>
-      <div></div>
+      <div className="bg-custom-bg bg-cover bg-center h-screen md:bg-center md:bg-cover sm:bg-auto sm:bg-top">
+        <Navbar />
+        <Header />
+
+        <WhitePage />
+      </div>
     </>
   );
 }
