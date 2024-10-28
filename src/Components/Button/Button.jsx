60606f29df6d3c84d420957b7d0333e0a5932444
dodimarthen <dodimartin.sit@gmail.com>
2024-10-28 14:41:12 +0700

    create button component

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = () => {
   return (
     <button
-      className="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      className="rounded-md bg-transparent py-2 px-4 border border-white-800 text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:shadow-none active:shadow-none hover:bg-slate-700 hover:text-white focus:bg-slate-700 active:bg-slate-700 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
       type="button"
     >
       Button
