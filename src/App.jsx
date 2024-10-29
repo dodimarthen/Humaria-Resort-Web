ad2a1973ea8525dfebaf0883607ab650b1215bfe
dodimarthen <dodimartin.sit@gmail.com>
2024-10-29 16:50:08 +0700

    try to implement image slider for background header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Components/Header/Header";
-import WhitePage from "./Components/WhitePage/WhitePage";
-import Navbar from "./Components/Navbar/navbar";
+import Navbar from "./Components/Navbar/Navbar";
+import HealingDestination from "./Components/WhitePage/WhitePage";
+
+// Array of image URLs
+const backgroundImages = ["/img/TobaLake.jpg", "/img/toba2.jpg"];
 
 function App() {
-  return (
-    <>
-      <div className="bg-custom-bg bg-cover bg-center h-screen md:bg-center md:bg-cover sm:bg-auto sm:bg-top">
-        <Navbar />
-        <Header />
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-        <WhitePage />
-      </div>
-    </>
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex(
+        (prevIndex) => (prevIndex + 1) % backgroundImages.length
+      );
+    }, 10000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div
+      className="bg-cover bg-center h-screen md:bg-center md:bg-cover sm:bg-auto sm:bg-top transition-all duration-1000 ease-in-out"
+      style={{
+        backgroundImage: `url(${backgroundImages[currentImageIndex]})`,
+      }}
+    >
+      <Navbar />
+      <Header />
+      <HealingDestination />
+    </div>
   );
 }
 
