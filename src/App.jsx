3354c06d8c1def998442c6df232035adc59b6490
dodimarthen <dodimartin.sit@gmail.com>
2024-10-30 13:51:11 +0700

    applying new navbar and new ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,13 @@
-import React, { useState, useEffect } from "react";
-import Header from "./Components/Header/Header";
-import Navbar from "./Components/Navbar/Navbar";
-import HealingDestination from "./Components/WhitePage/WhitePage";
-
-// Array of image URLs
-const backgroundImages = ["/img/TobaLake.jpg", "/img/toba2.jpg"];
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
 
 function App() {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentImageIndex(
-        (prevIndex) => (prevIndex + 1) % backgroundImages.length
-      );
-    }, 10000);
-
-    return () => clearInterval(intervalId);
-  }, []);
-
   return (
-    <div
-      className="bg-cover bg-center h-screen md:bg-center md:bg-cover sm:bg-auto sm:bg-top transition-all duration-1000 ease-in-out"
-      style={{
-        backgroundImage: `url(${backgroundImages[currentImageIndex]})`,
-      }}
-    >
+    <div className="min-h-screen bg-white dark:bg-[#181C14] transition-colors duration-300">
       <Navbar />
-      <Header />
-      <HealingDestination />
+      <main className="text-gray-700 dark:text-gray-200 p-8">
+        <Header />
+      </main>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
         "custom-bg": "url('/img/TobaLake.jpg')",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+        amatic: ["Amatic SC", "cursive"],
+        badscript: ["Bad Script", "cursive"],
       },
     },
   },
