a505fe7689ab420c72fdd34a3e53cf09c54bd24c
dodimarthen <dodimartin.sit@gmail.com>
2024-10-31 11:12:05 +0700

    modify button

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 const Button = () => {
   return (
-    <button
-      className="rounded-md bg-transparent py-2 px-4 border border-white-800 text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:shadow-none active:shadow-none hover:bg-slate-700 hover:text-white focus:bg-slate-700 active:bg-slate-700 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-      type="button"
-    >
-      Button
-    </button>
+    <div className="mb-10">
+      <button
+        className="rounded-full bg-[#DBD3D3] py-3 px-4 border border-slate-800 text-center text-sm text-slate-800 transition-all shadow-md hover:shadow-lg focus:shadow-none active:shadow-none hover:bg-slate-700 hover:text-white focus:bg-slate-700 active:bg-slate-700 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+        type="button"
+      >
+        Book Now
+      </button>
+    </div>
   );
 };
 
