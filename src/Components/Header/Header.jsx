76c5691bfe1dfe0e398a2d9cd20bbc234d5e29c4
dodimarthen <dodimartin.sit@gmail.com>
2024-10-31 17:20:10 +0700

    adding header components

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,15 @@
-// src/components/Header.jsx
 import React from "react";
 const Header = () => {
   return (
-    <div>
-      <h1 className="text-3xl">Humaria Resort</h1>
-      <p className="mt-4">Samosir Island, North Sumatra</p>
+    <div className="bg-custom-header bg-cover bg-center w-screen h-[90vh] flex flex-col justify-center items-center">
+      <div className="">
+        <h1 className="font-badscript text-[#FFF7F1] text-2xl sm:text-base md:text-2xl lg:text-4xl">
+          Humaria Resort
+        </h1>
+        <p className="mt-4 text-xl sm:text-xl md:text-4xl lg:text-6xl text-[#FFF7F1]">
+          Explore Toba With Us.
+        </p>
+      </div>
     </div>
   );
 };
