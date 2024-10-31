b9391150e5a6138a0b886fe51cc292b4d899b525
dodimarthen <dodimartin.sit@gmail.com>
2024-10-31 11:11:17 +0700

    make navlink more responsive

--- src/Components/Navlink/Navlink.jsx
+++ src/Components/Navlink/Navlink.jsx
@@ -2,9 +2,10 @@ function NavLink({ href, children }) {
   return (
     <a
       href={href}
-      className="text-lg text-gray-700 dark:text-gray-200 pb-1 border-b-4 border-transparent hover:border-[#29ADB2] dark:hover:border-[#FFE4C9] transition-colors duration-300"
+      className="inline-block max-w-max pt-2 text-sm sm:text-base md:text-md lg:text-md text-gray-700 dark:text-gray-200 pb-0.5 border-b-2 border-transparent hover:border-[#EB8317] dark:hover:border-[#B4B4B3] transition-colors duration-200 relative"
     >
       {children}
+      <span className="absolute left-0 right-0 bottom-0 h-0.25 bg-[#EB8317] dark:bg-[#B4B4B3] rounded-full transform scale-x-0 hover:scale-x-100 transition-transform duration-300 origin-left"></span>
     </a>
   );
 }
