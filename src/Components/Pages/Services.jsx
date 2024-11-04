f344f1a2d6f11868025daf396649c477da9a119c
dodimarthen <dodimartin.sit@gmail.com>
2024-11-04 10:05:15 +0700

    remove bg transtion

--- src/Components/Pages/Services.jsx
+++ src/Components/Pages/Services.jsx
@@ -5,23 +5,23 @@ import CardComponent from "../Card/Card";
 const Services = () => {
   const servicesData = [
     {
-      imageSrc: "img/test.jpg",
-      title: "Hygiene Place",
+      imageSrc: "img/housekeeping.jpg",
+      title: "House keeping",
     },
     {
-      imageSrc: "img/toba2.jpg",
-      title: "Spa & Wellness",
+      imageSrc: "img/spa.jpg",
+      title: "Relaxing Spa",
     },
     {
-      imageSrc: "img/TobaLake.jpg",
-      title: "Fitness Center",
+      imageSrc: "img/campfire.jpg",
+      title: "Campfire ",
     },
   ];
 
   return (
     <div
       id="services"
-      className="flex flex-col items-center min-h-screen bg-custom-white dark:bg-[#191717] p-10 text-justify font-poppins"
+      className="flex flex-col min-h-screen items-center  py-4 text-justify font-poppins"
     >
       <h1 className="text-5xl text-gray-900 dark:text-custom-white">
         Our Services
