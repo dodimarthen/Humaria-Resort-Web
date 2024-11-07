ffdd92cb66a62caaf9ce6946e151abf8c90fdcdc
dodimarthen <dodimartin.sit@gmail.com>
2024-11-07 15:46:20 +0700

    Resize the card size

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -7,20 +7,11 @@ export default function CardComponent({ imageSrc, title }) {
       <img
         alt={title}
         src={imageSrc}
-        style={{ width: "50vh", height: "200px", objectFit: "cover" }}
+        style={{ width: "25vh", height: "200px", objectFit: "cover" }}
       />
 
-      <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-        <p className="text-tiny text-white/80 mr-20">{title}</p>
-        <Button
-          className="text-tiny text-white bg-black/20"
-          variant="flat"
-          color="default"
-          radius="lg"
-          size="sm"
-        >
-          Read More
-        </Button>
+      <CardFooter className="before:bg-white/10 border-white/20 border-1 overflow-hidden py-2 px-4 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+        <p className="text-tiny text-white/80">{title}</p>
       </CardFooter>
     </Card>
   );
