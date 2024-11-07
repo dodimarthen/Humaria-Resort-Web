de5249b7942361ad26ce9a2317a1393074abb520
dodimarthen <dodimartin.sit@gmail.com>
2024-11-07 15:45:48 +0700

    Resize the card size

--- src/Components/Card/CardDestination.jsx
+++ src/Components/Card/CardDestination.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { FaMapMarkerAlt } from "react-icons/fa"; // Import location icon from react-icons
+import { FaMapMarkerAlt } from "react-icons/fa";
 
 const CardDestination = ({ image, title, distance, days, bgColor }) => {
   return (
     <div className={`${bgColor} rounded-lg shadow-md p-2`}>
       <div className="w-full h-48 overflow-hidden rounded-lg">
         {" "}
-        <img src={image} alt={title} className="w-[50vh] h-full object-cover" />
+        <img src={image} alt={title} className="w-[27vh] h-full object-cover" />
       </div>
       <div className="mt-4">
         <h3 className="text-lg font-semibold text-white">{title}</h3>
